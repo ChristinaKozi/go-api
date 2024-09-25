837de6a9b103c5e08d6b70665e90cd96a0ab77ec
Christina Kozanian <christinakozanian@gmail.com>
2024-09-25 13:51:20 -0700

    adds controller functions and routes

---
+++ controllers/user.controller.go
@@ -0,0 +1,45 @@
+package controllers
+
+import (
+	"github.com/ChristinaKozi/go-api/services"
+	"github.com/gin-gonic/gin"
+)
+
+type UserController struct {
+	UserService services.UserService
+}
+
+func New(userservice services.UserService) UserController {
+	return UserController{
+		UserService: userservice,
+	}
+}
+
+func (uc *UserController) CreateUser(ctx *gin.Context) {
+	ctx.JSON(200, "")
+}
+
+func (uc *UserController) GetUser(ctx *gin.Context) {
+	ctx.JSON(200, "")
+}
+
+func (uc *UserController) GetAll(ctx *gin.Context) {
+	ctx.JSON(200, "")
+}
+
+func (uc *UserController) UpdateUser(ctx *gin.Context) {
+	ctx.JSON(200, "")
+}
+
+func (uc *UserController) DeleteUser(ctx *gin.Context) {
+	ctx.JSON(200, "")
+}
+
+func (uc *UserController) RegisterUserRoutes(rg *gin.RouterGroup) {
+	userroute := rg.Group("/user")
+	userroute.POST("/create", uc.CreateUser)
+	userroute.GET("/get/:name", uc.GetUser)
+	userroute.GET("/getall", uc.GetAll)
+	userroute.PATCH("/update", uc.UpdateUser)
+	userroute.DELETE("/delete", uc.DeleteUser)
+}
